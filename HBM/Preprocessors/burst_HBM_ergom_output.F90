481bd79b9565fdf9f90b7af088a7663f32c63de9
Asbjorn Christensen <asc@aqua.dtu.dk>
2017-05-23 09:58:25 +0200

    implemented new DMI binary format; new/old format busrt selected by preprocessor directive

--- HBM/Preprocessors/burst_HBM_ergom_output.F90
+++ HBM/Preprocessors/burst_HBM_ergom_output.F90
@@ -8,7 +8,7 @@
 !          p : means the file is a physics file (no name pattern parsing)
 !          b : means the file is a biology file (no name pattern parsing)
 !       and
-!          prop ==  t|s|u|v|z for physics layers or z1|z2|ch  for biology layers (more to follow)
+!          prop ==  t|s|u|v|z for physics layers or (see list below) for biology layers
 !
 !       (for windows users the executable will be burstHBM.exe)  
 !
@@ -22,10 +22,22 @@
 !
 !              ------------- biology layers -------------------------
 !
-!              z1 : extract micro zooplankton (3D)
-!              z2 : extract meso  zooplankton (3D)
-!              ch : extract chlorophyll (3D)
-!        
+!              nh4  : extract NH4  (3D)               
+!              no3  : extract NO3 (3D)
+!              po4  : extract PO4 (3D)
+!              sio4 : extract SiO4 (3D)
+!              dia  : extract diatoms (3D)
+!              fla  : extract flagellates (3D)
+!              cya  : extract cyano bacteria  (3D)
+!              zo1  : extract micro zooplankton (3D)
+!              zo2  : extract meso  zooplankton (3D)
+!              spm1 : extract suspended particulate matters in content of nitrogen (3D)
+!              spm2 : extract suspended particulate matters in content of silicate (3D)
+!              oxy  : extract oxygen tension (3D)
+!              chl  : compose chlorophyll (3D)          
+!              sed1 : extract sedimant variable 1 (2D)
+!              sed2 : extract sedimant variable 1 (2D)
+!          
 !       The current version does not consistently return non-zero for anormal execusions (but info printet to stdout is updated)  
 !       ----------------------------------------------------------------------------------------------------------
 !       Based on HBM reading example cmod2cdf.f90/bio2cdf.f90, provided by Zhenwen Wan [zw@dmi.dk] May 26, 2015
@@ -56,20 +68,25 @@
 !       
 !       2 sediment variables refer to as
 !                   SPM_sed1(i,j)=sed(1,id3d(jm+1-j,i,1))/scale               
-!                   SPM_sed2(i,j)=sed(2,id3d(jm+1-j,i,1))/scale
+!                             SPM_sed2(i,j)=sed(2,id3d(jm+1-j,i,1))/scale
 !
-!       -------------------------------------- updates --------------------------------------
-!
-!       May 18, 2017: DMI dump format has changed, so unformatted files need to be opened with access='stream'         
-!          
+!       May 18, 2017: DMI dump format has changed, so unformatted files need to be opened with access='stream'
+!                     and indexing in eco/benthic variables was switched
+!                     Read old format by defining preprocessor flag OLDFORMAT (default: new format)
+!         
 !========================================================================================================================  
 !       Linux build : 
-!            ifort -o burstHBM -convert big_endian -e90 -I/usr/local/include -L/usr/local/lib -lnetcdff -lnetcdf  burst_HBM_ergom_output.f90
-!
+!            ifort -o burstHBM -convert big_endian -I/usr/local/include -L/usr/local/lib -lnetcdff -lnetcdf  burst_HBM_ergom_output.F90
+!          
+!            gfortran -o burstHBM     -fconvert=big-endian             burst_HBM_ergom_output.F90 -I/usr/include -L/usr/lib -lnetcdff -lnetcdf -lnetcdf -lnetcdff
+!            gfortran -o burstHBM_old -fconvert=big-endian -DOLDFORMAT burst_HBM_ergom_output.F90 -I/usr/include -L/usr/lib -lnetcdff -lnetcdf -lnetcdf -lnetcdff
+!          
 !       cross-compile to static windows executable: e.g.
 !            NETCDF=/home/asbjorn/DTU/Ballastvand_SRA/IBMlib_port_to_windows/mingw_netcdf/working/NETCDF
-!            /usr/bin/x86_64-w64-mingw32-gfortran -c -fconvert=big-endian -I${NETCDF}/include burst_HBM_ergom_output.f90
-!            /usr/bin/x86_64-w64-mingw32-gfortran -static -o burstHBM.exe burst_HBM_ergom_output.o -L${NETCDF}/lib  -lnetcdf -lnetcdff -lnetcdf -lnetcdff
+!            /usr/bin/x86_64-w64-mingw32-gfortran -c -DOLDFORMAT -fconvert=big-endian -I${NETCDF}/include burst_HBM_ergom_output.F90
+!            /usr/bin/x86_64-w64-mingw32-gfortran -static -o burstHBM_old.exe burst_HBM_ergom_output.o -L${NETCDF}/lib  -lnetcdf -lnetcdff -lnetcdf -lnetcdff
+!            /usr/bin/x86_64-w64-mingw32-gfortran -c -fconvert=big-endian -I${NETCDF}/include burst_HBM_ergom_output.F90
+!            /usr/bin/x86_64-w64-mingw32-gfortran -static -o burstHBM.exe     burst_HBM_ergom_output.o -L${NETCDF}/lib  -lnetcdf -lnetcdff -lnetcdf -lnetcdff          
 !========================================================================================================================
         use netcdf   
         implicit None
@@ -88,7 +105,9 @@
         integer(4),dimension(0:n2d3nm2),target :: wu2,wv2,zz2
         integer(4),dimension(0:n2d3nm3),target :: wu3,wv3,zz3
 !       ---- ecovars ----
-        integer,parameter :: neco=12, nsed=2  ! 
+        integer,parameter :: neco=12, nsed=2  !
+#ifdef OLDFORMAT
+!       --- old data layout
         integer(4),dimension(neco,0:n3d3nm0),target :: eco3nm0
         integer(4),dimension(neco,0:n3d3nm1),target :: eco3nm1
         integer(4),dimension(neco,0:n3d3nm2),target :: eco3nm2
@@ -97,6 +116,20 @@
         integer(4),dimension(nsed,0:n2d3nm1),target :: sed3nm1
         integer(4),dimension(nsed,0:n2d3nm2),target :: sed3nm2
         integer(4),dimension(nsed,0:n2d3nm3),target :: sed3nm3
+        character*(*), parameter :: ecofmttyp = "old"
+#else
+!       --- new data layout (after May 2017)       
+        integer(4),dimension(0:n3d3nm0,neco),target :: eco3nm0
+        integer(4),dimension(0:n3d3nm1,neco),target :: eco3nm1
+        integer(4),dimension(0:n3d3nm2,neco),target :: eco3nm2
+        integer(4),dimension(0:n3d3nm3,neco),target :: eco3nm3
+        integer(4),dimension(0:n2d3nm0,nsed),target :: sed3nm0
+        integer(4),dimension(0:n2d3nm1,nsed),target :: sed3nm1
+        integer(4),dimension(0:n2d3nm2,nsed),target :: sed3nm2
+        integer(4),dimension(0:n2d3nm3,nsed),target :: sed3nm3
+        character*(*), parameter :: ecofmttyp = "new"
+#endif
+        
 !       --------------------------------------------------
         real(4),allocatable :: buf_3d(:) 
         real(4),allocatable :: buf_2d(:)    
@@ -108,7 +141,7 @@
         character*(*), parameter     :: usage = "Usage: burstHBM [p|b] <physfile|biofile> [prop*]"
 
         character cdate*10,ctmp*19,btmp*11
-        character ncfname*999, prop*2, typ, action*24
+        character ncfname*999, prop*4, typ, action*24
         character*999 ::  ifilename
         real(4), parameter :: wuvscale = 1.0e7    ! wind scalefactor       -> m/s
         real(4), parameter :: uvscale  = 1.0e8    ! current scalefac.      -> m/s
@@ -117,7 +150,7 @@
         real(4), parameter :: zscale   = 1.0e8    ! elevation scfac.       -> m
         real(4), parameter :: bscale   = 1.0e4    ! for biology conversion -> mmolN/m3
 
-        integer :: ios, last, nfram, nargc, ia
+        integer :: ios, last, nfram, nargc, ia, ieco
         integer :: year,month,day,hour,minutes,seconds
         real(8) :: tmp
         logical :: readphys
@@ -204,15 +237,11 @@
 !       ----------------------------------------------------------------------
 
         call getarg(2, ifilename)         ! 0 is program name
-!        
-!       ... format prior to May 18, 2017, use this open statement
-!        
-!        open(11,file=ifilename,form='unformatted', status='old', iostat=ios)
-!        
-!       ... format after to May 18, 2017, use this open statement
-!         
-        open(11,file=ifilename,form='unformatted', status='old', iostat=ios, access='stream')
-        
+#ifdef OLDFORMAT
+        open(11,file=ifilename,form='unformatted', status='old', iostat=ios)
+#else
+        open(11,file=ifilename,form='unformatted', status='old', iostat=ios, access='stream') 
+#endif
         if(ios/=0) goto 800
         write(*,*) action, trim(adjustl(ifilename))
         
@@ -221,7 +250,8 @@
 !       ----------------------------------------------------------------------   
      
         do ia = 3, nargc    ! possibly void loop
-           call getarg(ia, prop) 
+           call getarg(ia, prop)
+           prop = adjustl(prop)
            write(*,*) "extracting ", prop, " from ", trim(adjustl(ifilename))
 !          ------ read header
            rewind(11)
@@ -245,70 +275,67 @@
               if (last==2) goto 850    ! last = 2: unexpected EOF
               
 !         ------ dump frames depending on selection ----   
-             
+
+              write(ncfname, 534) trim(prop), area, year,month,day,hour,minutes,seconds
               select case (prop)
 
-!             ------ physics ------
-              case ("t")
+ !             ------ physics ------
+                 
+              case ("t   ")
                  if (.not.readphys) goto 891
-                 write(ncfname, 534) "t_ns", year,month,day,hour,minutes,seconds
                  call get_t(buf_3d)
                  call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'deg.C')        
 
-              case ("s")
+              case ("s   ")
                  if (.not.readphys) goto 891
-                 write(ncfname, 534) "s_ns", year,month,day,hour,minutes,seconds
                  call get_s(buf_3d)
                  call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'PSU') 
                  
-              case ("u")
+              case ("u   ")
                  if (.not.readphys) goto 891
-                 write(ncfname, 534) "u_ns", year,month,day,hour,minutes,seconds
                  call get_u(buf_3d)
                  call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'east m/s') 
 
-              case ("v")
+              case ("v   ")
                  if (.not.readphys) goto 891
-                 write(ncfname, 534) "v_ns", year,month,day,hour,minutes,seconds
                  call get_v(buf_3d)
                  call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'north m/s') 
 
-              case ("z")
+              case ("z   ")
                  if (.not.readphys) goto 891
-                 write(ncfname, 534) "z_ns", year,month,day,hour,minutes,seconds
                  call get_z(buf_2d)   ! NB: 2D
                  call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_2d, 'm') 
 
 !             ------ biology ------
-              case ("z1")
+                 
+              case ("nh4 ", "no3 ", "po4 ", "sio4", "dia ", "fla ", "cya ", "zo1 ", "zo2 ", "spm1", "spm2", "oxy ")   ! direct eco variables
                  if (readphys) goto 890
-                 write(ncfname, 534) "zo1_ns", year,month,day,hour,minutes,seconds
-                 call get_zo1(buf_3d)
+                 call get_eco_index(prop, ieco)
+                 call get_eco_data(buf_3d, ieco)
                  call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'mmol.m-3') 
 
-              case ("z2")   
+              case ("sed1", "sed2")    ! direct sediment variables
                  if (readphys) goto 890
-                 write(ncfname, 534) "zo2_ns", year,month,day,hour,minutes,seconds
-                 call get_zo2(buf_3d)
-                 call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'mmol.m-3') 
-
-              case ("ch")
+                 call get_eco_index(prop, ieco)
+                 call get_sed_data(buf_3d, ieco)
+                 call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'mmol.m-3')
+                 
+              case ("chl ")        ! derived variable
                  if (readphys) goto 890
-                 write(ncfname, 534) "chl_ns", year,month,day,hour,minutes,seconds
                  call get_chl(buf_3d)
-                 call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'mmol.m-3') 
-
+                 call dump_frame_as_netcdf_float4(trim(adjustl(ncfname)), buf_3d, 'mmol.m-3')
+                 
               case default
                  goto 880
-                 
-                 
+                                  
            end select
+              
            enddo  ! frame loop
         enddo     ! loop over arguments
 
         close(11)
         goto 900  
-534     format(a,'_',i4.4,'_',i2.2,'_',i2.2,'_',i2.2,'_',i2.2,'_',i2.2,".nc")
+534     format(a,'_',a,'_',i4.4,'_',i2.2,'_',i2.2,'_',i2.2,'_',i2.2,'_',i2.2,".nc")
 
 !       ----------------------------------------------------------------------
 !       write exit messages
@@ -385,7 +412,7 @@
         read(11,end=700) ctmp,tmp
         read(ctmp,532) year,month,day,hour,minutes,seconds
 532     format(i4,1x,i2,1x,i2,1x,i2,1x,i2,1x,i2)
-        write(*,*) "read ergom frame ", ctmp, tmp
+        write(*,*) "read ergom frame ", ctmp, tmp, " layout = ", ecofmttyp
 !       ------ data frames ----          
         read(11,end=800) eco3nm0
         read(11,end=800) sed3nm0
@@ -453,86 +480,89 @@
         end subroutine get_wv
 
 !       ------ ERGOM ------
-
-        subroutine get_nh4(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(1,:)/bscale    ! include element 0 in array, explicit cast to real*4       
-        end subroutine get_nh4
-      
-        subroutine get_no3(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(2, :)/bscale   ! include element 0 in array, explicit cast to real*4        
-        end subroutine get_no3
-
-        subroutine get_po4(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(3, :)/bscale   ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_po4
         
-        subroutine get_Sio4(buf)
+        subroutine get_eco_data(buf, i)
         real(4), intent(out) :: buf(:)
-        buf = eco3nm(4, :)/bscale   ! include element 0 in array, explicit cast to real*4        
-        end subroutine get_Sio4
-             
-        subroutine get_dia(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(5, :)/bscale  ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_dia
-
-        subroutine get_fla(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(6, :)/bscale  ! include element 0 in array, explicit cast to real*4        
-        end subroutine get_fla
-
-        subroutine get_cya(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(7, :)/bscale  ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_cya
-
-        subroutine get_zo1(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(8, :)/bscale   ! include element 0 in array, explicit cast to real*4        
-        end subroutine get_zo1
-
-        subroutine get_zo2(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(9, :)/bscale   ! include element 0 in array, explicit cast to real*4        
-        end subroutine get_zo2
-
-        subroutine get_SPM1(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(10, :)/bscale  ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_SPM1
-
-        subroutine get_SPM2(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(11, :)/bscale  ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_SPM2
-
-        subroutine get_oxy(buf)
-        real(4), intent(out) :: buf(:)
-        buf = eco3nm(12, :)/bscale  ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_oxy
+        integer, intent(in)  :: i
+#ifdef OLDFORMAT
+        buf = eco3nm(i, :)/bscale   ! include element 0 in array, explicit cast to real*4
+#else
+        buf = eco3nm(:, i)/bscale   ! include element 0 in array, explicit cast to real*4
+#endif       
+        end subroutine get_eco_data
 
 
-        subroutine get_SPM_sed1(buf)
+        subroutine get_sed_data(buf, i)
         real(4), intent(out) :: buf(:)
-        buf =  sed3nm(1, :)/bscale  ! include element 0 in array, explicit cast to real*4         
-        end subroutine get_SPM_sed1
-      
-        subroutine get_SPM_sed2(buf)
-        real(4), intent(out) :: buf(:)
-        buf =  sed3nm(2, :)/bscale   ! include element 0 in array, explicit cast to real*4        
-        end subroutine get_SPM_sed2
-       
+        integer, intent(in)  :: i
+#ifdef OLDFORMAT
+        buf = sed3nm(i, :)/bscale   ! include element 0 in array, explicit cast to real*4
+#else
+        buf = sed3nm(:, i)/bscale   ! include element 0 in array, explicit cast to real*4
+#endif
+        end subroutine get_sed_data
+        
 !       ------ derived variables ------
 
         subroutine get_chl(buf)
         real(4), intent(out) :: buf(:)
-        buf = (eco3nm(5,:) + eco3nm(6,:) + eco3nm(7,:))/bscale         ! include element 0 in array, explicit cast to real*4  
+        integer :: i1,i2,i3
+        call get_eco_index("dia ", i1)
+        call get_eco_index("fla ", i2)
+        call get_eco_index("cya ", i3)
+#ifdef OLDFORMAT
+        buf = (eco3nm(i1,:) + eco3nm(i2,:) + eco3nm(i3,:))/bscale  ! include element 0 in array, explicit cast to real*4
+#else
+        buf = (eco3nm(:,i1) + eco3nm(:,i2) + eco3nm(:,i3))/bscale  ! include element 0 in array, explicit cast to real*4
+#endif
         end subroutine get_chl
-        
 
+
+        
+        subroutine get_eco_index(p, ie)
+!       -----------------------------------------------------
+!       implement the index mapping of eco + spm variables
+!       -----------------------------------------------------        
+        character*4, intent(in) :: p
+        integer, intent(out)    :: ie
+        character*4 :: prop4
+        prop4 = adjustl(p)
+        select case (p)
+!       ---- eco vars
+        case("nh4 ")
+           ie = 1                    
+        case("no3 ")
+           ie = 2
+        case("po4 ")
+           ie = 3               
+        case("sio4")
+           ie = 4               
+        case("dia ")
+           ie = 5
+        case("fla ")
+           ie = 6
+        case("cya ")
+           ie = 7
+        case("zo1 ")
+           ie = 8
+        case("zo2 ")
+           ie = 9
+        case("spm1")
+           ie = 10  ! suspended particulate matters in content of nitrogen
+        case("spm2")
+           ie = 11  ! suspended particulate matters in content of silicate
+        case("oxy ")
+           ie = 12            
+!       ---- sediment vars
+        case("sed1")
+           ie = 1
+        case("sed2")
+           ie = 2
+        case default
+           write(*,*) "property ", p, " is not mappable"
+           stop 742
+        end select   
+        end subroutine get_eco_index
 
 
         subroutine dump_frame_as_netcdf_float4(FILE_NAME, var, unit)
